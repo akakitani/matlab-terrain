d0e723c3ab409cfd403e3cb899fa16e805bf9a09
akakitani <atsunori.kakitani@gmail.com>
2014-11-23 18:12:25 -0800

    Add smoothing for visualization

--- generate_terrain.m
+++ generate_terrain.m
@@ -1,22 +1,21 @@
-    clear
-    clf
-
-    SIZE = (2 ^ 5) + 1;
+function generate_terrain(size, detail, gen_function)
+    SIZE = (2 ^ size) + 1;
     MAX_HEIGHT = SIZE;
-    DETAIL = 0.05;
-    heightmap = midpoint_displacement(SIZE, MAX_HEIGHT);
+    DETAIL = detail;
+    heightmap = gen_function(SIZE, MAX_HEIGHT);
 
     [X,Y] = meshgrid(1:SIZE, 1:SIZE);
     [Xq, Yq] = meshgrid(1:DETAIL:SIZE, 1:DETAIL:SIZE);
 
-    smoothed = interp2(X, Y, heightmap, Xq, Yq);
-    pcolor(Xq, Yq, smoothed);
-    mesh(smoothed);
+    interpolated_heightmap = interp2(X, Y, heightmap, Xq, Yq);
+    pcolor(Xq, Yq, interpolated_heightmap);
 
+    y = filter2(fspecial('gaussian'), interpolated_heightmap);
+    mesh(Xq, Yq, conv2(interpolated_heightmap, y, 'same'));
     xlabel('x');
     ylabel('y');
     view([+34.5 14]);
-
     drawnow;
 
     fprintf('Simulation finished.\n\n');
+end
---
+++ main.m
@@ -0,0 +1,1 @@
+generate_terrain(5, 0.2, @midpoint_displacement)
