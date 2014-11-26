41db9133d1130c9294aaa65537a44bc69de82da3
akakitani <atsunori.kakitani@gmail.com>
2014-11-26 15:39:18 -0800

    Add DLA

--- main.m
+++ main.m
@@ -2,14 +2,22 @@ clear
 clf reset
 cla reset
 
-
+%{
 SIZE_LEVEL = 6;
 DETAIL_LEVEL = 20;
 WATER_LEVEL = 0.45;
-Z_ASPECT_RATIO = 0.05;
-SMOOTHING = true;
-
+Z_ASPECT_RATIO = 0.1;
+SMOOTHING = false;
 heightmap = generate_terrain(SIZE_LEVEL, @midpoint_displacement);
+render(heightmap, DETAIL_LEVEL, WATER_LEVEL, Z_ASPECT_RATIO, SMOOTHING)
+%}
+
+SIZE_LEVEL = 9;
+DETAIL_LEVEL = 1;
+WATER_LEVEL = 0.1;
+Z_ASPECT_RATIO = 0.01;
+SMOOTHING = true;
+heightmap = generate_terrain(SIZE_LEVEL, @dla);
 render(heightmap, DETAIL_LEVEL, WATER_LEVEL, Z_ASPECT_RATIO, SMOOTHING)
 
 
